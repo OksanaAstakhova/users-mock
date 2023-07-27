60970b36073878e5b676910627dc8c36f1e532c1
Oksana Astakhova <oksana.astakhova@okami.tech>
2023-07-27 13:51:22 +0300

    fixed

--- api/index.js
+++ api/index.js
@@ -1,44 +1,36 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-
-app.get("/", (req, res) => {
+module.exports = (req, res) => {
     res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
     res.json([
-        {
-            userEmail: "ivan.ponomarenko@in.com",
-            userLocation: "Луганська область",
-        },
-        {
-            userEmail: "igor.petrenko@in.com",
-            userLocation: "Одеська область",
-        },
-        {
-            userEmail: "olena.tkachenko@in.com",
-            userLocation: "Кіровоградська область",
-        },
-        {
-            userEmail: "oksana.astakhova@in.com",
-            userLocation: "Сумська область",
-        },
-        { userEmail: "sergii.gulenok@in.com", userLocation: "м. Київ" },
-        {
-            userEmail: "oleksandr.ponomarov@in.com",
-            userLocation: "м. Київ",
-        },
-        {
-            userEmail: "oleksii.ponomarov@in.com",
-            userLocation: "м. Київ",
-        },
-        {
-            userEmail: "liza.gorielikova@in.com",
-            userLocation: "Луганська область",
-        },
+        [
+            {
+                userEmail: "ivan.ponomarenko@in.com",
+                userLocation: "Луганська область",
+            },
+            {
+                userEmail: "igor.petrenko@in.com",
+                userLocation: "Одеська область",
+            },
+            {
+                userEmail: "olena.tkachenko@in.com",
+                userLocation: "Кіровоградська область",
+            },
+            {
+                userEmail: "oksana.astakhova@in.com",
+                userLocation: "Сумська область",
+            },
+            { userEmail: "sergii.gulenok@in.com", userLocation: "м. Київ" },
+            {
+                userEmail: "oleksandr.ponomarov@in.com",
+                userLocation: "м. Київ",
+            },
+            {
+                userEmail: "oleksii.ponomarov@in.com",
+                userLocation: "м. Київ",
+            },
+            {
+                userEmail: "liza.gorielikova@in.com",
+                userLocation: "Луганська область",
+            },
+        ],
     ]);
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
-
-module.exports = app;
+};
