88f5d64ff73b0fa5a0dbf83ad499d7f9eff1e0c1
Oksana Astakhova <oksana.astakhova@okami.tech>
2023-07-27 13:52:47 +0300

    fixed

--- api/index.js
+++ api/index.js
@@ -1,36 +1,31 @@
-module.exports = (req, res) => {
-    res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-    res.json([
-        [
-            {
-                userEmail: "ivan.ponomarenko@in.com",
-                userLocation: "Луганська область",
-            },
-            {
-                userEmail: "igor.petrenko@in.com",
-                userLocation: "Одеська область",
-            },
-            {
-                userEmail: "olena.tkachenko@in.com",
-                userLocation: "Кіровоградська область",
-            },
-            {
-                userEmail: "oksana.astakhova@in.com",
-                userLocation: "Сумська область",
-            },
-            { userEmail: "sergii.gulenok@in.com", userLocation: "м. Київ" },
-            {
-                userEmail: "oleksandr.ponomarov@in.com",
-                userLocation: "м. Київ",
-            },
-            {
-                userEmail: "oleksii.ponomarov@in.com",
-                userLocation: "м. Київ",
-            },
-            {
-                userEmail: "liza.gorielikova@in.com",
-                userLocation: "Луганська область",
-            },
-        ],
-    ]);
-};
+[
+    {
+        userEmail: "ivan.ponomarenko@in.com",
+        userLocation: "Луганська область",
+    },
+    {
+        userEmail: "igor.petrenko@in.com",
+        userLocation: "Одеська область",
+    },
+    {
+        userEmail: "olena.tkachenko@in.com",
+        userLocation: "Кіровоградська область",
+    },
+    {
+        userEmail: "oksana.astakhova@in.com",
+        userLocation: "Сумська область",
+    },
+    { userEmail: "sergii.gulenok@in.com", userLocation: "м. Київ" },
+    {
+        userEmail: "oleksandr.ponomarov@in.com",
+        userLocation: "м. Київ",
+    },
+    {
+        userEmail: "oleksii.ponomarov@in.com",
+        userLocation: "м. Київ",
+    },
+    {
+        userEmail: "liza.gorielikova@in.com",
+        userLocation: "Луганська область",
+    },
+];
