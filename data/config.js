471492c1b047bd798e5d92c0fbafcefc05e71aa5
Oksana Astakhova <oksana.astakhova@okami.tech>
2023-08-23 18:00:56 +0300

    changed timeout

--- data/config.js
+++ data/config.js
@@ -1,12 +1,16 @@
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 
-const PORT = process.env.PORT || 3000
-const DOMAIN = process.env.DOMAIN
-const JOB_DURATION = 30 * 1000
-const APP_ID = process.env.APP_ID
-const SECRET = process.env.SECRET
+const PORT = process.env.PORT || 3000;
+const DOMAIN = process.env.DOMAIN;
+const JOB_DURATION = 10 * 1000;
+const APP_ID = process.env.APP_ID;
+const SECRET = process.env.SECRET;
 
 module.exports = {
-  PORT, DOMAIN, JOB_DURATION, APP_ID, SECRET
-}
+    PORT,
+    DOMAIN,
+    JOB_DURATION,
+    APP_ID,
+    SECRET,
+};
